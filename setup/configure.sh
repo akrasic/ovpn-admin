cfbb437fd60787bb8e09f588c99b0b1c02f4e7d4
Antun Krasic <antun@martuna.co>
2025-12-19 16:35:19 +0000

    Update configure.sh

--- setup/configure.sh
+++ setup/configure.sh
@@ -4,36 +4,40 @@ set -ex
 EASY_RSA_LOC="/etc/openvpn/easyrsa"
 SERVER_CERT="${EASY_RSA_LOC}/pki/issued/server.crt"
 
-OVPN_SRV_NET=${OVPN_SERVER_NET:-172.16.100.0}
-OVPN_SRV_MASK=${OVPN_SERVER_MASK:-255.255.255.0}
+OVPN_SRV_NET="${OVPN_SERVER_NET:-172.16.100.0}"
+OVPN_SRV_MASK="${OVPN_SERVER_MASK:-255.255.255.0}"
 
-
-cd $EASY_RSA_LOC
+cd "$EASY_RSA_LOC"
 
 if [ -e "$SERVER_CERT" ]; then
   echo "Found existing certs - reusing"
 else
-  if [ ${OVPN_ROLE:-"master"} = "slave" ]; then
+  if [[ "${OVPN_ROLE:-master}" == "slave" ]]; then
     echo "Waiting for initial sync data from master"
-    while [ $(wget -q localhost/api/sync/last/try -O - | wc -m) -lt 1 ]
+    while [ "$(wget -q --timeout=10 localhost/api/sync/last/try -O - | wc -m)" -lt 1 ]
     do
       sleep 5
     done
   else
     echo "Generating new certs"
     easyrsa --batch init-pki
-    cp -R /usr/share/easy-rsa/* $EASY_RSA_LOC/pki
+    cp -R /usr/share/easy-rsa/* "$EASY_RSA_LOC/pki"
     echo "ca" | easyrsa build-ca nopass
     easyrsa --batch build-server-full server nopass
     easyrsa gen-dh
-    openvpn --genkey --secret ./pki/ta.key
+    openvpn --genkey secret ./pki/ta.key
   fi
 fi
 easyrsa gen-crl
 
-iptables -t nat -D POSTROUTING -s ${OVPN_SRV_NET}/${OVPN_SRV_MASK} ! -d ${OVPN_SRV_NET}/${OVPN_SRV_MASK} -j MASQUERADE || true
-iptables -t nat -A POSTROUTING -s ${OVPN_SRV_NET}/${OVPN_SRV_MASK} ! -d ${OVPN_SRV_NET}/${OVPN_SRV_MASK} -j MASQUERADE
+# Enable IP forwarding
+echo 1 > /proc/sys/net/ipv4/ip_forward
 
+# Setup NAT for VPN clients
+iptables -t nat -D POSTROUTING -s "${OVPN_SRV_NET}/${OVPN_SRV_MASK}" ! -d "${OVPN_SRV_NET}/${OVPN_SRV_MASK}" -j MASQUERADE || true
+iptables -t nat -A POSTROUTING -s "${OVPN_SRV_NET}/${OVPN_SRV_MASK}" ! -d "${OVPN_SRV_NET}/${OVPN_SRV_MASK}" -j MASQUERADE
+
+# Create TUN device if needed
 mkdir -p /dev/net
 if [ ! -c /dev/net/tun ]; then
     mknod /dev/net/tun c 10 200
@@ -41,19 +45,28 @@ fi
 
 cp -f /etc/openvpn/setup/openvpn.conf /etc/openvpn/openvpn.conf
 
-if [ ${OVPN_PASSWD_AUTH} = "true" ]; then
+# Password authentication setup
+if [[ "${OVPN_PASSWD_AUTH:-false}" == "true" ]]; then
   mkdir -p /etc/openvpn/scripts/
   cp -f /etc/openvpn/setup/auth.sh /etc/openvpn/scripts/auth.sh
   chmod +x /etc/openvpn/scripts/auth.sh
-  echo "auth-user-pass-verify /etc/openvpn/scripts/auth.sh via-file" | tee -a /etc/openvpn/openvpn.conf
-  echo "script-security 2" | tee -a /etc/openvpn/openvpn.conf
-  echo "verify-client-cert require" | tee -a /etc/openvpn/openvpn.conf
-  openvpn-user db-init --db.path=$EASY_RSA_LOC/pki/users.db && openvpn-user db-migrate --db.path=$EASY_RSA_LOC/pki/users.db
+  echo "auth-user-pass-verify /etc/openvpn/scripts/auth.sh via-file" >> /etc/openvpn/openvpn.conf
+  echo "script-security 2" >> /etc/openvpn/openvpn.conf
+  echo "verify-client-cert require" >> /etc/openvpn/openvpn.conf
+  openvpn-user db-init --db.path="$EASY_RSA_LOC/pki/users.db" && openvpn-user db-migrate --db.path="$EASY_RSA_LOC/pki/users.db"
 fi
 
-[ -d $EASY_RSA_LOC/pki ] && chmod 755 $EASY_RSA_LOC/pki
-[ -f $EASY_RSA_LOC/pki/crl.pem ] && chmod 644 $EASY_RSA_LOC/pki/crl.pem
+# Set permissions
+[ -d "$EASY_RSA_LOC/pki" ] && chmod 755 "$EASY_RSA_LOC/pki"
+[ -f "$EASY_RSA_LOC/pki/crl.pem" ] && chmod 644 "$EASY_RSA_LOC/pki/crl.pem"
 
 mkdir -p /etc/openvpn/ccd
 
-openvpn --config /etc/openvpn/openvpn.conf --client-config-dir /etc/openvpn/ccd --port 1194 --proto tcp --management 127.0.0.1 8989 --dev tun0 --server ${OVPN_SRV_NET} ${OVPN_SRV_MASK}
+# Start OpenVPN
+exec openvpn --config /etc/openvpn/openvpn.conf \
+  --client-config-dir /etc/openvpn/ccd \
+  --port 1194 \
+  --proto tcp \
+  --management 127.0.0.1 8989 \
+  --dev tun0 \
+  --server "${OVPN_SRV_NET}" "${OVPN_SRV_MASK}"
